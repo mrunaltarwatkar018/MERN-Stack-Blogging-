8fe27f86a742c5c4c14892bb9bc48e6eddaa76fa
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-07 00:58:54 +0530

    Removing Unnecessary things like comments

--- blogging_website_frontend/src/components/navbar.component.jsx
+++ blogging_website_frontend/src/components/navbar.component.jsx
@@ -11,7 +11,6 @@ const Navbar = () => {
     const [ userNavPanel, setUserNavPanel ] = useState(false);
 
     const {userAuth, userAuth: {access_token, profile_img}} = useContext(UserContext);
-    // const { userAuth, userAuth: { access_token, profile_img}, setUserAuth } = useContext(UserContext);
     
     const handleUserNavPanel = () => {
         setUserNavPanel(currentVal => !currentVal);
@@ -37,7 +36,6 @@ const Navbar = () => {
                         className="w-full md:w-full bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
                     />
 
-                    {/* <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i> */}
                     <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
                 </div>
                 
@@ -96,83 +94,3 @@ const Navbar = () => {
 }
 
 export default Navbar;
-
-
-
-
-
-
-// import React, { useContext, useState } from 'react';
-// import { Link, Outlet } from 'react-router-dom';
-// import logo from "../imgs/logo.png";
-// import { UserContext } from '../App';
-
-// const Navbar = () => {
-
-//     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
-
-//     const { userAuth, userAuth: { access_token, profile_img }, setUserAuth } = useContext(UserContext);
-
-//     return (
-//         <>
-//             <nav className="navbar">
-//                 <Link to="/" className="flex-none w-10">
-//                     <img src={logo} className="full"/>
-//                 </Link>
-
-//                 <div className={"bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " + ( searchBoxVisibility ? "show" : "hide")}>
-//                     <input 
-//                         type="text"
-//                         placeholder="Search"
-//                         className="w-full md:w-full bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
-//                     />
-
-//                     {/* <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i> */}
-//                     <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
-//                 </div>
-                
-
-//                 <div className="flex items-center gap-3 md:gap-6 ml-auto">
-//                     <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
-//                     onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}>
-//                         <i className="fi fi-rr-search text-xl"></i>
-//                     </button>
-//                 </div>
-                
-//                 <Link to="/" className="hidden md:flex gap-2 link">
-//                     <i className="fi fi-rr-file-edit"></i>
-//                     <p>Write</p>
-//                 </Link>
-
-//                 {
-//                     access_token ? 
-//                     <>
-//                         <Link to="/dashboard/notifications" >
-//                             <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
-//                                 <i className="fi fi-rr-bell text-2xl block mt-2"></i>
-//                             </button>
-//                         </Link>
-//                     </> 
-//                     :
-//                     <>
-
-//                         <Link className="btn-dark py-2" to="/signin">
-//                             <p>Sign In</p>
-//                         </Link>
-
-//                         <Link className="btn-light py-2 hidden md:block" to="/signup">
-//                             <p>Sign Up</p>
-//                         </Link>
-
-//                     </>
-//                 }
-
-//             </nav>
-
-//             <Outlet />
-
-//         </>
-//     );
-// }
-
-// export default Navbar;

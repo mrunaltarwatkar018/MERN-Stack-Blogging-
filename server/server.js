253bf8194b18233c2efa1d3c9f9188b1610c9e0c
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-26 18:11:12 +0530

    Section 24 : "Create-blog" route

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ import aws from "aws-sdk";
 
 // schema below
 import User from "./Schema/User.js";
+import Blog from "./Schema/Blog.js";
 
 const server = express();
 
@@ -50,6 +51,25 @@ const generateUploadURL = async () => {
         ContentType: "image/jpeg",
     });
 };
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if ( token == null ) {
+        return res.status(401).json( { error: "No access token" } );
+    }
+    
+    jwt.verify(token, process.env.SECRET_ACCESS_KEY, (err, user) => {
+        if (err) {
+            // return res.status(403).json( { error: "Invalid access token" } );
+            return res.status(403).json({ error: "Access token is invalid" });
+        }
+
+        req.user = user.id;
+        next();
+    })
+}
 
 const formatdatatoSend = (user) => {
     const access_token = jwt.sign(
@@ -226,6 +246,71 @@ server.post("/goolgle-auth", async (req, res) => {
         return res.status(500).json({ "error": "Failed to authenticate you with google. Try with some other google account" });
     })
 })
+
+server.post("/create-blog", verifyJWT, (req, res) => {
+    
+    let authorId = req.user;
+
+    let { title, des, banner, tags, content, draft } = req.body;
+
+    if ( !title.length ) {
+        return res.status(403).json( { error: "You must provide a title to publish the blog" } );
+    }
+
+    if ( !des.length || des.length > 200 ) {
+        // return res.status(403).json( { error: "You must provide a description to publish the blog. The description must be less than 200 characters" } )
+        return res.status(403).json( { error: "You must provide blog description under 200 characters" } );
+    }
+
+    if ( !banner.length ) {
+        // return res.status(403).json( { error: "You must provide a banner to publish the blog" } )
+        return res.status(403).json( { error: "You must provide blog banner to publish the blog" } );
+    }
+    
+    if ( !content.blocks.length ) {
+        return res.status(403).json( {  error: "There must be some blog content to publish the blog" } );
+    }
+
+    if ( !tags.length || tags.length > 10 ) {
+        // return res.status(403).json({ error: "You must provide blog tags under 10 characters", });
+        return res.status(403).json( { error: "Provide tags in order to publish the blog, Maximum 10" } );
+    }
+
+    tags = tags.map((tag) => tag.toLowerCase());
+
+    let blog_id = title.replace(/[^a-zA-Z0-9]/g, " ").replace(/\s+/g, "-").trim() + nanoid();
+
+    let blog = new Blog( {
+        title, des, banner, content, tags, author: authorId, blog_id, draft: Boolean(draft)
+    } );
+
+    blog
+        .save()
+        .then((blog) => {
+            let incrementVal = draft ? 0 : 1;
+            // val = val + 1;
+
+            User.findOneAndUpdate(
+                { _id: authorId },
+                {
+                    $inc: { "account_info.total_posts": incrementVal },
+                    $push: { blogs: blog._id },
+                }
+            )
+            .then((user) => {
+                return res.status(200).json({ id: blog.blog_id });
+            })
+            .catch((err) => {
+                return res
+                .status(500)
+                .json({ error: "Failed to update total posts number" });
+            });
+        })
+        .catch((err) => {
+            return res.status(500).json({ error: err.message });
+        });
+} )
+
 
 server.listen(PORT, () => {
     console.log("listening on port --> " + PORT);

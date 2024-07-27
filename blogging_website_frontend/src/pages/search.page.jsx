2f18e47e16d397dcf4a33bd187e1807064d0a49f
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-27 18:31:58 +0530

    Section 42 - Search - User Card

--- blogging_website_frontend/src/pages/search.page.jsx
+++ blogging_website_frontend/src/pages/search.page.jsx
@@ -8,6 +8,7 @@ import NoDataMessage from "../components/nodata.component";
 import LoadMoreDataBtn from "../components/load-more.component";
 import axios from "axios";
 import { filterPaginationData } from "../common/filter-pagination-data";
+import UserCard from "../components/usercard.component";
 
 
 const SearchPage = () => {
@@ -56,6 +57,30 @@ const SearchPage = () => {
         setUsers(null);
     }
 
+    const UserCardWrapper = () => {
+        return (
+            <>
+
+                {
+
+                    users == null ? <Loader /> :
+                        users.length ?
+                        users.map((user, i) => {
+                            return <AnimationWrapper key={i} transition={ { duration: 1, delay: i*0.08 } } >
+                                <UserCard user={user} />
+                            </AnimationWrapper>
+                        } )
+
+                    :
+
+                    <NoDataMessage message="No user found" />
+
+                }
+
+            </>
+        )
+    }
+
     return (
 
         <section className="h-cover flex justify-center gap-10" >
@@ -63,6 +88,8 @@ const SearchPage = () => {
             <div className="w-full" >
 
                 <InPageNavigation routes={ [`Search results for "${query}"`, "Accounts Matched"] } defaultHidden={ ["Accounts Matched"] } >
+
+                        <>
 
                             {
 
@@ -83,10 +110,27 @@ const SearchPage = () => {
 
                             <LoadMoreDataBtn state={blogs} fetchDataFun={searchBlogs} />
 
+                        </>
+
+                        <UserCardWrapper />
+
                 </InPageNavigation>
 
             </div>
 
+            <div className="min-w-[40%] lg:min-w-[350px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden" >
+
+                <h1 className="font-medium text-xl mb-8">
+
+                    User related to search
+
+                    <i className="fi fi-rr-user mt-1"></i>
+
+                </h1>
+
+                <UserCardWrapper />
+
+            </div>
 
         </section>
 

0433d33b7479661fb0dd657be3a8bf67b0d7aa34
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-27 17:51:59 +0530

    Section 40 : Search Page

--- server/server.js
+++ server/server.js
@@ -294,9 +294,15 @@ server.get("/trending-blogs", ( req, res ) => {
 } )
 
 server.post("/search-blogs", (req, res) => {
-    let { tag, page } = req.body;
+    let { tag, query, page } = req.body;
 
-    let findQuery = { tags: tag, draft: false };
+    let findQuery;
+
+    if ( tag ) {
+        findQuery = { tags: tag, draft: false }
+    } else if (query) {
+        findQuery = { draft: false, title: new RegExp(query, "i" ) }
+    }
 
     let maxLimit = 2;
 
@@ -316,9 +322,15 @@ server.post("/search-blogs", (req, res) => {
 
 server.post("/search-blogs-count", (req, res) => {
 
-    let { tag } = req.body;
+    let { tag, query } = req.body;
 
-    let findQuery = { tags: tag, draft: false };
+    let findQuery;
+
+    if ( tag ) {
+        findQuery = { tags: tag, draft: false }
+    } else if (query) {
+        findQuery = { draft: false, title: new RegExp(query, "i" ) }
+    }
 
     Blog.countDocuments(findQuery)
         .then( (count) => {

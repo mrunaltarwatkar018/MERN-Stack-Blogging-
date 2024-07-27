57e429e314cf802d60f627fcda206d56c56e41c1
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-27 16:37:55 +0530

    Section 38 : Working Search Input

--- blogging_website_frontend/src/components/navbar.component.jsx
+++ blogging_website_frontend/src/components/navbar.component.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import logo from "../imgs/logo.png";
 import { UserContext } from '../App';
 import { Navigate } from 'react-router-dom';
@@ -10,10 +10,21 @@ const Navbar = () => {
     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
     const [ userNavPanel, setUserNavPanel ] = useState(false);
 
+    let navigate = useNavigate();
+
     const {userAuth, userAuth: {access_token, profile_img}} = useContext(UserContext);
     
     const handleUserNavPanel = () => {
         setUserNavPanel(currentVal => !currentVal);
+    }
+
+    const handleSearch = (e) => {
+        let query = e.target.value;
+        
+        if(e.keyCode == 13 && query.length){
+            navigate(`/search/${query}`);
+        }
+
     }
 
     const handleBlur = () => {
@@ -34,6 +45,7 @@ const Navbar = () => {
                         type="text"
                         placeholder="Search"
                         className="w-full md:w-full bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
+                        onKeyDown={handleSearch}
                     />
 
                     <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>

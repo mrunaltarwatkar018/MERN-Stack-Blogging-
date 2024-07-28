adcf13794596b61afa00022ba0f96ab16b40d1a1
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-28 14:50:07 +0530

    Section 50 : Fetching Blog Details

--- blogging_website_frontend/src/pages/blog.page.jsx
+++ blogging_website_frontend/src/pages/blog.page.jsx
@@ -1,12 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+export const blogStructure = {
+    title: "",
+    des: "",
+    content: [],
+    tags: [],
+    author: { personal_info: {  } },
+    banner: "",
+    publishedAt: "",
+}
 
 const BlogPage = () => {
 
     let { blog_id } = useParams();
 
+    let [ blog, setBlog ] = useState(blogStructure);
+
+    let { title, content, banner, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
+
+    const fetchBlog = () => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", {  blog_id } )
+            .then( ( { data: { blog } } ) => {
+                setBlog(blog);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
+    useEffect( () => {
+        fetchBlog();
+    }, [] )
+
     return (
-        <h1>This is a Blog Page for - { blog_id } </h1>
+        <h1>This is a Blog Page for - { blog.title } </h1>
     )
 }
 

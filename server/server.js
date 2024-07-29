ac6af350eb9a2dee115a6853518e7c39bde1093c
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-30 02:04:50 +0530

    Section 57 : Comments - Creating Comments in DB

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ import aws from "aws-sdk";
 import User from "./Schema/User.js";
 import Blog from "./Schema/Blog.js";
 import Notification from "./Schema/Notification.js";
+import Comment from "./Schema/Comment.js";
 
 const server = express();
 
@@ -523,6 +524,58 @@ server.post("/isliked-by-user", verifyJWT, (req, res) => {
     Notification.exists({ user: user_id, type: "like", blog: _id })
         .then ( result => { return res.status(200).json({ result }) } )
         .catch( err => { return res.status(500).json({ error: err.message }) } )
+})
+
+server.post("/add-comment", verifyJWT, (req, res) => {
+
+    let user_id = req.user;
+
+    let { _id, comment, blog_author } = req.body;
+
+    if ( !comment.length ) {
+        // return res.status(400).json({ error: "Comment cannot be empty" });
+        return res.status(400).json({ error: "Write something to leave a comment" });
+    }
+
+    //creating a comment doc
+    let commentObj = new Comment({ blog_id: _id, blog_author, comment, commented_by: user_id })
+
+    commentObj.save().then( commentFile => {
+
+        let { comment, commentedAt, children } = commentFile;
+
+        Blog.findOneAndUpdate( 
+            { _id }, 
+            { 
+                $push: { "comments": commentFile._id }, 
+                $inc: { 
+                    "activity.total_comments": 1, 
+                    "activity.total_parent_comments": 1, 
+                } 
+            } 
+        )
+        .then( blog => { console.log( "New comment created" ) } );
+
+        let notificationObj = {
+            type: "comment",
+            blog: _id,
+            notification_for: blog_author,
+            user: user_id,
+            comment: commentFile._id,
+        }
+
+        new Notification(notificationObj).save().then( notification => console.log( "New notification created" ) );
+
+        return res.status(200).json({
+            comment,
+            commentedAt,
+            _id: commentFile._id,
+            user_id,
+            children,
+        })
+
+    })
+
 })
 
 server.listen(PORT, () => {
